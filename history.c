0973256294ddb631a402f548e50fa522eb07c236
mialsy <cwang128@dons.usfca.edu>
2021-04-01 12:49:00 -0700

    fixed hist get

--- history.c
+++ history.c
@@ -42,6 +42,7 @@ void hist_init(unsigned int limit)
     }
 }
 
+
 void hist_destroy(void)
 {   
     if (list != NULL) {
@@ -67,50 +68,8 @@ struct iterator create_iter(void)
     return it;
 }
 
-const char *iterate(struct iterator *iter) {
-    const char *ptr = hist_search_cnum(list->insertions - iter->idx - 1);
-    iter->idx++;
-    return ptr;
-}
 
-const char *iterate_rev(struct iterator *iter) {
-    if (list->insertions < list->capacity) {
-        return hist_search_cnum(iter->idx++);
-    } else {
-        const char *ptr = hist_search_cnum(iter->idx + list->insertions - list->capacity);
-        iter->idx++;
-        return ptr;
-    }
-}
-
-void hist_print(void)
-{
-    // print history
-    const char *elem;
-    struct iterator iter = create_iter();
-    while ((elem = iterate_rev(&iter)) != NULL) {
-        printf("%*zu %s", 3, iter.idx, elem);
-    }
-}
-
-const char *hist_search_prefix(char *prefix)
-{
-    // Retrieves the most recent command starting with 'prefix', or NULL
-    // if no match found.
-
-    struct iterator iter = create_iter();
-    const char *elem;
-    while ((elem = iterate(&iter)) != NULL)
-    {
-        if (strncmp(prefix, elem, strlen(prefix)) == 0) {
-            return elem;
-        }
-    }
-    
-    return NULL;
-}
-
-const char *hist_search_cnum(int command_number)
+const char *get_idx(int command_number)
 {
     // Retrieves a particular command number. Return NULL if no match found.
     if (list == NULL || command_number >= list->insertions || ((list->insertions > list->capacity) && (command_number < list->insertions - list->capacity)))
@@ -123,7 +82,72 @@ const char *hist_search_cnum(int command_number)
     return list->cmd_storage + idx * MAX_CMD_LEN;
 }
 
+const char *iterate(struct iterator *iter) {
+    const char *ptr = get_idx(list->insertions - iter->idx - 1);
+    iter->idx++;
+    return ptr;
+}
+
+const char *iterate_rev(struct iterator *iter) {
+    if (list->insertions < list->capacity) {
+        return get_idx(iter->idx++);
+    } else {
+        const char *ptr = get_idx(iter->idx + list->insertions - list->capacity);
+        iter->idx++;
+        return ptr;
+    }
+}
+
+void hist_print(void)
+{
+    // print history
+    const char *elem;
+    struct iterator iter = create_iter();
+    while ((elem = iterate_rev(&iter)) != NULL) {
+        printf("%*zu %s\n", 3, iter.idx, elem);
+    }
+}
+
+const char *hist_search_cnum(int command_num) {
+    return get_idx(command_num + list->insertions - list->capacity - 1);
+}
+
+const char *hist_search_prefix(char *prefix)
+{
+    // Retrieves the most recent command starting with 'prefix', or NULL
+    // if no match found.
+    struct iterator iter = create_iter();
+    const char *elem;
+    while ((elem = iterate(&iter)) != NULL)
+    {
+        if (strncmp(prefix, elem, strlen(prefix)) == 0) {
+            return elem;
+        }
+    }
+    
+    return NULL;
+}
+
 unsigned int hist_last_cnum(void)
 {
-    return list->insertions;
+    return list->insertions < list->capacity ? list->insertions : list->capacity;
 }
+
+// int main(void) {
+
+//     // test 
+//     hist_init(5);
+//     hist_add("ls");
+//     hist_add("pwd");
+//     hist_add("ls -alt");
+//     hist_add("cd main");
+//     hist_add("wc -l");
+//     hist_add("history");
+//     hist_add("ls -r");
+
+//     hist_print();
+//     printf("last: %zu\n", hist_last_cnum());
+//     char *pre = "pwd";
+//     printf("last ls: %s\n", hist_search_prefix(pre));
+//     printf("cmd 4: %s\n", hist_search_cnum(4));
+// }
